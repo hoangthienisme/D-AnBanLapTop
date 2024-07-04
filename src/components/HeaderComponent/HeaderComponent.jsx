c508930ec2f83ddb8ec77cbe04e026ac0ae01d33
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-07-05 01:29:23 +0700

    Xây dựng UI cho phần Header

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
-
+import { Col, } from 'antd';
+import { WrapperHeader, WrapperHomeHeader, WrapperMainHeader, WrapperSubHeader, WrapperTextHeader } from './style';
+import Search from 'antd/es/transfer/search';
+import {
+  HomeOutlined, SmileOutlined,ShoppingCartOutlined
+} from '@ant-design/icons';
 const HeaderComponent = () => {
   return (
     <div>
-      Header
+      <WrapperHeader>
+        <Col span={4} >
+          <WrapperTextHeader>
+            <WrapperMainHeader>TIKI</WrapperMainHeader>
+            <WrapperSubHeader>Tốt & Nhanh</WrapperSubHeader>
+          </WrapperTextHeader>
+        </Col>
+        <Col span={14} style={{ opacity: '0.5' }}>
+          <Search placeholder="Bạn tìm gì hôm nay " enterButton />
+          <span style={{ marginLeft: '10px' }}>
+            <a href="#">Điện gia dụng </a>
+            <a href="#"> xe cộ </a>
+            <a href="#"> mẹ & bé </a>
+            <a href="#"> khỏe đẹp </a>
+            <a href="#"> nhà cửa </a>
+            <a href="#"> sách </a>
+            <a href="#"> thể thao </a>
+            <a href="#"> máy lọc nước </a>
+            <a href="#"> máy vắt cam </a>
+          </span>
+        </Col>
+        <Col span={6} >
+          <WrapperHomeHeader>
+            <HomeOutlined />
+            Trang Chủ
+            <span style={{ margin: '12px' }}>
+              <SmileOutlined /> Tài Khoản      |
+              <span style={{ margin: '12px' }}>
+              <ShoppingCartOutlined />
+              </span>
+              
+            </span>
+            
+            </WrapperHomeHeader>
+        </Col>
+      </WrapperHeader>
     </div>
   )
 }
---
+++ src/components/HeaderComponent/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import {Row} from 'antd'
+export const WrapperHeader= styled(Row)`
+        padding:10px 120px;
+        
+`
+export const WrapperTextHeader=styled.div`
+        font-size: 15px;
+        color: blue;
+        line-height:1;
+        font-weigh: bold;
+        text-align: left;
+        width: 150%;
+`        
+export const WrapperMainHeader=styled.span`
+        font-size:55px;
+        font-weigh:bold;
+        display:block;
+`
+export const WrapperSubHeader=styled.span`
+        font-size:13px;
+        display:block;
+        width: 300px;
+        
+`
+export const WrapperHomeHeader=styled.span`
+        text-align:right;
+        font-size:16px;
+        line-height: 2.5;
+        color:blue;
+`
